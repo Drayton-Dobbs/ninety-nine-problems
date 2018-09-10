0d052e7a850bfac790da61ae9065709325329ee7
Drayton-Dobbs <jamesdraytondobbs@gmail.com>
2018-09-10 16:47:59 -0500

    some message

---
+++ script.js
@@ -0,0 +1,8 @@
+var friends = ["Tyler", "Jacob", "Justin" , "Jeremi", "Maggie"]
+
+for (let i = 0; i < friends.length; i++) {
+    for (let j = 99; j > 0; j--) {
+        console.log([j] + " lines of code in the file, " + [j] + " lines of code. " + friends[i] + " strikes one out, clears it all out, " + [j] + " lines of code in the file");
+    }
+}
+ 
