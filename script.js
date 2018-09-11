d30a68f4302f4546c7e72d207accd580dc46d239
Drayton-Dobbs <jamesdraytondobbs@gmail.com>
2018-09-11 11:03:40 -0500

    got it

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var friends = ["Tyler", "Jacob", "Justin" , "Jeremi", "Maggie"]
+var friends = ["Tyler", "Jacob", "Justin", "Jeremi", "Maggie"]
 
 for (let i = 0; i < friends.length; i++) {
+    var getFriend = friends[i]
+    console.log(friends[i])
     for (let j = 99; j > 0; j--) {
-        console.log([j] + " lines of code in the file, " + [j] + " lines of code. " + friends[i] + " strikes one out, clears it all out, " + [j] + " lines of code in the file");
+        if (j === 2) {
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + getFriend + " strikes one out, clears it all out, " + (j-1) + " line of code in the file")
+        } else if (j === 1) {
+            console.log(j + " line of code in the file, " + j + " line of code; " + getFriend + " strikes one out, clears it all out, no more lines of code in the file")
+        } else {
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + getFriend + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file")
+        }
     }
 }
- 
